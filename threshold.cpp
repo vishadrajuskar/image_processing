ce6198c87ba7d4fdffaabb1696ca35bdef714195
prashant <prashantchikhalkar@gmail.com>
2016-09-10 23:25:51 +0530

    Update threshold.cpp

--- threshold.cpp
+++ threshold.cpp
@@ -1,3 +1,25 @@
+/**
+  **Thresholding**
+
+Thresholding is the simplest method of image segmentation.
+
+From a grayscale image, thresholding can be used to create binary images
+
+*How it works ?*
+
+The simplest thresholding methods replace in an image with a black pixel if the 
+image intensity I(i, j) is less than some fixed constant T (that is, I(i,j) < T) or a white pixel if the image intensity is greater than that constant.
+
+*Types of thresholding*
+
+1. Global
+
+2. Adaptive
+
+3. Histogram based
+
+ * */
+
 #include "stdafx.h"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
